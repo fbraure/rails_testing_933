49c5e6b195713e077ad0e207410b6a9f71827050
Florent BRAURE <florent.braure@gmail.com>
2022-09-01 20:13:12 +0200

    run unit and system tests on GHA

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "full_name" do
+    user = User.new(first_name: "jOhn", last_name: "LeNON")
+    assert_equal user.full_name, "John LENON"
+  end
 end
